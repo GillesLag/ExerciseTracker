959d1f657dcd3548644df938869327bec743210e
Gilles Lagrilliere <gilles.lagrilliere@codraft.be>
2025-02-03 17:20:01 +0100

    Added RunSessionController

---
+++ ExerciseTracker/Controlllers/RunSessionController.cs
@@ -0,0 +1,29 @@
+﻿using ExerciseTracker.Repositories;
+using ExerciseTracker.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseTracker.Controlllers;
+
+internal class RunSessionController
+{
+    private readonly RunSessionRepository _repository;
+    private readonly RunSessionService _service = new RunSessionService();
+    public RunSessionController(RunSessionRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public void AddRunSession(DateTime start, DateTime end, int meters)
+    {
+        var session = _service.CreateSession(start, end, meters);
+
+        if (_repository.Add(session) == 0)
+        {
+            throw new Exception();
+        }
+    }
+}
---
+++ ExerciseTracker/Services/RunSessionService.cs
@@ -0,0 +1,24 @@
+﻿using ExerciseTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseTracker.Services;
+
+internal class RunSessionService
+{
+    public RunSession CreateSession(DateTime start, DateTime end, int distanceInMeters)
+    {
+        var session = new RunSession()
+        {
+            DateStart = start,
+            DateEnd = end,
+            Duration = end - start,
+            DistanceInMeter = distanceInMeters
+        };
+
+        return session;
+    }
+}
