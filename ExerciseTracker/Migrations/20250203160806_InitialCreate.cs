d020d9050243b448cc465b079798a67f31c40519
Gilles Lagrilliere <gilles.lagrilliere@codraft.be>
2025-02-03 17:08:48 +0100

    Added database

---
+++ ExerciseTracker/Migrations/20250203160806_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExerciseTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RunSessions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DistanceInMeter = table.Column<int>(type: "INTEGER", nullable: false),
+                    DateStart = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateEnd = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Duration = table.Column<TimeSpan>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RunSessions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RunSessions");
+        }
+    }
+}
--- ExerciseTracker/Repositories/RunSessionRepository.cs
+++ ExerciseTracker/Repositories/RunSessionRepository.cs
@@ -1,11 +1,6 @@
 ﻿using ExerciseTracker.Data;
 using ExerciseTracker.Models;
 using ExerciseTracker.Repositories.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExerciseTracker.Repositories;
 
